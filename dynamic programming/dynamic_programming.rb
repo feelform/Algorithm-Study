6f0bc3d63b782774a5ac7858acc8fad7bc7a089f
yongseok kang <yongseok.kang@navercorp.com>
2018-04-25 10:40:39 +0900

    Add IS

--- dynamic programming/dynamic_programming.rb
+++ dynamic programming/dynamic_programming.rb
@@ -21,19 +21,17 @@ def incr_subseq(seq)
   l = {}              # => {}
 
   seq.length.times do |j|               # => 3
-    l_conn_keys = l.keys.select do |i|  # => [], [], []
-      i < j and seq[i] < seq[j]
-    end                                 # => [], [], []
-    
-    l_conn_keys.each do |i|  # => [], [], []
-      l[i] = l_conn_keys
-    end                      # => [], [], []
+    l_conn_keys = l.keys.select do |i|  # => [],   [0],  [0, 1]
+      i < j and seq[i] < seq[j]         # => true, true, true
+    end                                 # => [],   [0],  [0, 1]
 
-    l  # => {}, {}, {}
-  end  # => 3
+    l_conn_values = l_conn_keys.map { |k| seq[k] }  # => [], [1], [1, 3]
 
-  return l  # => {}
+    l[j] = l_conn_values ? l_conn_values.push(seq[j]) : [seq[j]]  # => [1], [1, 3], [1, 3, 5]
+  end                                                             # => 3
+
+  return l  # => {0=>[1], 1=>[1, 3], 2=>[1, 3, 5]}
 end         # => :incr_subseq
 
-incr_subseq([1, 3, 5])  # => {}
+incr_subseq([1, 3, 5])  # => {0=>[1], 1=>[1, 3], 2=>[1, 3, 5]}
 
