04cc997ef0fc727403a2edcb58a41fca226d3aff
yongseok kang <yongseok.kang@navercorp.com>
2018-05-16 12:20:37 +0900

    Add return foods list

--- combination/allergies.rb
+++ combination/allergies.rb
@@ -8,33 +8,38 @@ class Allergies
     @values[@values.keys.length] = friends  # => ["dara", "minzy"], ["cl", "minzy"]
   end                                       # => :add
 
-  def list()
-    menu = {}  # => {}
+  def available_foods()
+    menu = {}            # => {}, {}
 
-    friends = @values.values.flatten.uniq                                           # => ["dara", "minzy", "cl"]
-    @values.keys.each { |key|                                                       # => [0, 1]
-      menu[key] = {}                                                                # => {},                      {}
-      friends.each { |friend|                                                       # => ["dara", "minzy", "cl"], ["dara", "minzy", "cl"]
-        menu[key][friend] = @values[key].select { |value| value == friend }.length  # => 1, 1, 0, 0, 1, 1
-      }                                                                             # => ["dara", "minzy", "cl"], ["dara", "minzy", "cl"]
-    }                                                                               # => [0, 1]
+    friends = @values.values.flatten.uniq                                           # => ["dara", "minzy", "cl"], ["dara", "minzy", "cl"]
+    @values.keys.each { |key|                                                       # => [0, 1],                  [0, 1]
+      menu[key] = {}                                                                # => {},                      {},                      {},                      {}
+      friends.each { |friend|                                                       # => ["dara", "minzy", "cl"], ["dara", "minzy", "cl"], ["dara", "minzy", "cl"], ["dara", "minzy", "cl"]
+        menu[key][friend] = @values[key].select { |value| value == friend }.length  # => 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, ...
+      }                                                                             # => ["dara", "minzy", "cl"], ["dara", "minzy", "cl"], ["dara", "minzy", "cl"], ["dara", "minzy", "cl"]
+    }                                                                               # => [0, 1], [0, 1]
 
-    (1..@values.keys.length).each { |food_index|             # => 1..2
-      @values.keys.combination(food_index).select { |foods|  # => #<Enumerator: [0, 1]:combination(1)>, #<Enumerator: [0, 1]:combination(2)>
+    (1..@values.keys.length).each { |food_index|             # => 1..2, 1..2
+      @values.keys.combination(food_index).select { |foods|  # => #<Enumerator: [0, 1]:combination(1)>, #<Enumerator: [0, 1]:combination(2)>, #<Enumerator: [0, 1]:combination(1)>, #<Enumerator: [0, 1]:combination(2)>
 
-        available_friends = foods.flat_map { |food|  # => [0], [1], [0, 1]
-          menu[food].select {|k,v| v > 0}.keys       # => ["dara", "minzy"], ["minzy", "cl"], ["dara", "minzy"], ["minzy", "cl"]
-        }.uniq                                       # => ["dara", "minzy"], ["minzy", "cl"], ["dara", "minzy", "cl"]
+        available_friends = foods.flat_map { |food|  # => [0], [1], [0, 1], [0], [1], [0, 1]
+          menu[food].select {|k,v| v > 0}.keys       # => ["dara", "minzy"], ["minzy", "cl"], ["dara", "minzy"], ["minzy", "cl"], ["dara", "minzy"], ["minzy", "cl"], ["dara", "minzy"], ["minzy", "cl"]
+        }.uniq                                       # => ["dara", "minzy"], ["minzy", "cl"], ["dara", "minzy", "cl"], ["dara", "minzy"], ["minzy", "cl"], ["dara", "minzy", "cl"]
 
-        if friends == available_friends  # => false, false, true
-          return foods                   # => [0, 1]
+        if friends == available_friends  # => false, false, true, false, false, true
+          return foods                   # => [0, 1], [0, 1]
         end
-      }                                  # => []
+      }                                  # => [], []
     }
-  end                                    # => :list
-end                                      # => :list
+  end                                    # => :available_foods
 
-allergies = Allergies.new()       # => #<Allergies:0x00007fb6d69480b0 @values={}>
-allergies.add(["dara", "minzy"])  # => ["dara", "minzy"]
-allergies.add(["cl", "minzy"])    # => ["cl", "minzy"]
-allergies.list()                  # => [0, 1]
+  def count_available_foods()
+    available_foods().length   # => 2
+  end                          # => :count_available_foods
+end                            # => :count_available_foods
+
+allergies = Allergies.new()        # => #<Allergies:0x00007ff9fd144570 @values={}>
+allergies.add(["dara", "minzy"])   # => ["dara", "minzy"]
+allergies.add(["cl", "minzy"])     # => ["cl", "minzy"]
+allergies.available_foods()        # => [0, 1]
+allergies.count_available_foods()  # => 2
