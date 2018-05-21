bfc704a964e467a95cc2222ee464698e25b6b82c
yongseok kang <yongseok.kang@navercorp.com>
2018-05-21 12:18:46 +0900

    Add search.rb

--- search/search.rb
+++ search/search.rb
@@ -1,14 +1,25 @@
 def linear_search(array, key)
-  if array.index(key).nil?                        # => false
+  if array.index(key).nil?
     return -1
   else
-    return "#{key} at index #{array.index(key)}"  # => "3 at index 6"
+    return "#{key} at index #{array.index(key)}"
   end
-end                                               # => :linear_search
+end
 
-arr = [7, 6, 25, 19, 8, 14, 3, 16, 2, 0]  # => [7, 6, 25, 19, 8, 14, 3, 16, 2, 0]
-key = 3                                   # => 3
-p linear_search(arr, key)                 # => "3 at index 6"
+arr = [7, 6, 25, 19, 8, 14, 3, 16, 2, 0]
+key = 3
+p linear_search(arr, key)
 
-# >> "3 at index 6"
 
+puts "Test " if true
+
+a = %w( ant bee cat dog elk )
+a.each { |animal| puts animal }
+
+['cat', 'dog', 'horse'].each do |animal|
+  print animal, " -- "
+end
+
+5.times { print "*" }
+3.upto(6) { |i| print i }
+('a'..'e').each { |char| print char }
