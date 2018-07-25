2f0a4e23a77e568fb48a570f0e1b8056e927cb67
yongseok kang <yongseok.kang@navercorp.com>
2018-07-25 16:14:43 +0900

    Add file

--- lib/prefix.rb
+++ lib/prefix.rb
@@ -1,47 +1,56 @@
-require 'set'
+require 'set'  # => true
 
 def prefix(m)
-  m.times { | index |
-    prefixes = $dic.select { |prefix| prefix.length >= index + 1 }.map {|word| word[0, index + 1] }
+  m.times { | index |                                                                                # => 4, 3
+    prefixes = $dic.select { |prefix| prefix.length >= index + 1 }.map {|word| word[0, index + 1] }  # => ["d", "d", "d", "d", "d", "d"], ["da", "da", "da", "do", "do", "do"], ["dar", "dat", "dav", "doc", "doc", "doc"], ["dark", "date", "dave", "dock"], ["a", "a", "a", "a", "b", "c", "c"], ["ab", "ab", "ac", "ac", "bc", "cb"], ["acc", "cba"]
     
-    prefixes_keys = prefixes|[]
-    prefixes_values = prefixes_keys.map { |key| prefixes.count(key)  }
+    prefixes_keys = prefixes|[]                                         # => ["d"], ["da", "do"], ["dar", "dat", "dav", "doc"], ["dark", "date", "dave", "dock"], ["a", "b", "c"], ["ab", "ac", "bc", "cb"], ["acc", "cba"]
+    prefixes_values = prefixes_keys.map { |key| prefixes.count(key)  }  # => [6],   [3, 3],       [1, 1, 1, 3],                 [1, 1, 1, 1],                     [4, 1, 2],       [2, 2, 1, 1],             [1, 1]
     
-    index = prefixes_values.index(prefixes_values.max)
+    index = prefixes_values.each_index.max_by { |i| prefixes_values[i] }  # => 0, 0, 3, 0, 0, 0, 0
 
-    puts prefixes_keys[index]
-  }
+    puts prefixes_keys[index]  # => nil, nil, nil, nil, nil, nil, nil
+  }                            # => 4, 3
   
-end
-=begin
+end  # => :prefix
+
 def run()
-  gets.to_i.times { |i|
+  gets.to_i.times { |i|                                # => 0
     $dic = []
     word_count, prefix_count = gets.split.map(&:to_i)
     word_count.times { $dic.push(gets.chomp)}
     prefix(prefix_count)
-  }
-end
+  }                                                    # => 0
+end                                                    # => :run
 
-run()
+run()  # => 0
 
-require 'benchmark'
+require 'benchmark'  # => true
 
-puts Benchmark.measure {
+puts Benchmark.measure {  # => Benchmark
 
-$dic = ["dark", "date", "dave", "doc", "doc", "dock"]
-prefix(4)
+$dic = ["dark", "date", "dave", "doc", "doc", "dock"]  # => ["dark", "date", "dave", "doc", "doc", "dock"]
+prefix(4)                                              # => 4
 
 
-$dic = ["ab", "ab", "ac", "acc", "bc", "c", "cba"]
-prefix(3)
+$dic = ["ab", "ab", "ac", "acc", "bc", "c", "cba"]  # => ["ab", "ab", "ac", "acc", "bc", "c", "cba"]
+prefix(3)                                           # => 3
 
-}
-=end
-$dic = ["dark", "date", "dave", "doc", "doc", "dock"]
-prefix(4)
+}  # => nil
+
+#$dic = ["dark", "date", "dave", "doc", "doc", "dock"]  # => ["dark", "date", "dave", "doc", "doc", "dock"]
+#prefix(4)                                              # => 4
 
 
 #$dic = ["ab", "ab", "ac", "acc", "bc", "c", "cba"]
 #prefix(3)
+
+# >> d
+# >> da
+# >> doc
+# >> dark
+# >> a
+# >> ab
+# >> acc
+# >>   0.000472   0.000262   0.000734 (  0.000560)
 
