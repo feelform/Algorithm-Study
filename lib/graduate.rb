6398d0a3fda8f5c566c9c4a4b017f19f1e88f1d8
Yongseok Kang <feelform@gmail.com>
2018-07-25 12:02:34 +0900

    graduate.rb

--- lib/graduate.rb
+++ lib/graduate.rb
@@ -1,27 +1,51 @@
 
 
-FRUIT = { 1 => 'Orange', 2 => 'Banana', 4 => 'Cherry', 8 => 'Apple' }  # => {1=>"Orange", 2=>"Banana", 4=>"Cherry", 8=>"Apple"}
+FRUIT = { 1 => 'Orange', 2 => 'Banana', 4 => 'Cherry', 8 => 'Apple' }
 
 def mask_fruit(a, b)
-  a | b               # => 9
-end                   # => :mask_fruit
+  a | b
+end
 
 def unmask_fruit(masked)
-  FRUIT.select { |k, _| (masked | k) == masked }  # => {1=>"Orange", 8=>"Apple"}
-end                                               # => :unmask_fruit
+  FRUIT.select { |k, _| (masked | k) == masked }
+end
 
-mask = mask_fruit 1, 8  # => 9
-unmask_fruit mask       # => {1=>"Orange", 8=>"Apple"}
+mask = mask_fruit 1, 8
+unmask_fruit mask
 
+INF = 999999
+
+def bit_count(value)
+  value.to_s(2).chars.select { |one| one == '1' }.size
+end
 
 def graduate(semester, taken)
-  N + 1                        # => 5, 6
-end                            # => :graduate
+  if bit_count(semester) >= K
+    return taken
+  end
 
-N = 4  # => 4
+  if taken >= M
+    return  INF
+  end
 
-graduate(10, 1)  # => 5
+  value = INF
+
+  
+end
+
+N = 4
+K = 4
+M = 4
+L = 4
+graduate(10, 1)
 
 
-N = 5            # => 5
-graduate(11, 2)  # => 6
+N = 5
+graduate(11, 2)
+
+bit_count(1)
+bit_count(2)
+bit_count(3)
+bit_count(5)
+bit_count(6)
+bit_count(7)
