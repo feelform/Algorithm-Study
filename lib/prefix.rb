31a3eab12233e0bf6bb808df05e77549d7eb4973
yongseok kang <yongseok.kang@navercorp.com>
2018-07-25 15:40:00 +0900

    refactor algorithm

--- lib/prefix.rb
+++ lib/prefix.rb
@@ -1,20 +1,18 @@
-require 'set'  # => true
+require 'set'
 
 def prefix(m)
-  m.times { | index |                                                                                # => 4
-    prefixes = $dic.select { |prefix| prefix.length >= index + 1 }.map {|word| word[0, index + 1] }  # => ["d", "d", "d", "d", "d", "d"], ["da", "da", "da", "do", "do", "do"], ["dar", "dat", "dav", "doc", "doc", "doc"], ["dark", "date", "dave", "dock"]
-    prefixes_keys = prefixes|[]                                                                      # => ["d"],                          ["da", "do"],                         ["dar", "dat", "dav", "doc"],               ["dark", "date", "dave", "dock"]
+  m.times { | index |
+    prefixes = $dic.select { |prefix| prefix.length >= index + 1 }.map {|word| word[0, index + 1] }
     
-    prefixes_values = prefixes_keys.map { |key| prefixes.count(key)  }  # => [6], [3, 3], [1, 1, 1, 3], [1, 1, 1, 1]
-
-    max = prefixes_values.max  # => 6, 3, 3, 1
+    prefixes_keys = prefixes|[]
+    prefixes_values = prefixes_keys.map { |key| prefixes.count(key)  }
     
-    index = prefixes_values.index(prefixes_values.max)  # => 0, 0, 3, 0
+    index = prefixes_values.index(prefixes_values.max)
 
-    puts prefixes_keys[index]  # => nil, nil, nil, nil
-  }                            # => 4
+    puts prefixes_keys[index]
+  }
   
-end                                                    # => :prefix
+end
 =begin
 def run()
   gets.to_i.times { |i|
@@ -40,15 +38,10 @@ prefix(3)
 
 }
 =end
-$dic = ["dark", "date", "dave", "doc", "doc", "dock"]  # => ["dark", "date", "dave", "doc", "doc", "dock"]
-prefix(4)                                              # => 4
+$dic = ["dark", "date", "dave", "doc", "doc", "dock"]
+prefix(4)
 
 
 #$dic = ["ab", "ab", "ac", "acc", "bc", "c", "cba"]
 #prefix(3)
-
-# >> d
-# >> da
-# >> doc
-# >> dark
 
