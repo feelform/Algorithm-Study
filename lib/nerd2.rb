333393316dd16c59f6da8dd271d606ed76d49bae
yongseok kang <yongseok.kang@navercorp.com>
2018-08-09 18:08:24 +0900

    Add nerd2

--- lib/nerd2.rb
+++ lib/nerd2.rb
@@ -1,3 +1,4 @@
+
 def contain?(point)
   index = $sorted_nerds.bsearch_index {|pi| point["pi"] < pi }
   if $sorted_nerds.length == 0 || index == nil
@@ -19,14 +20,16 @@ def add(point)
 end
 
 def remove(point) 
-  index = $sorted_nerds.bsearch_index {|pi| point["pi"] < pi}
+  index = $sorted_nerds.bsearch_index {|pi|
+    (point["pi"] > pi && point["qi"] > $nerds[pi])
+  }
 
   if index == nil
     return
   end
 
-  $sorted_nerds.slice(0, index)
-  left_nerds = $sorted_nerds.slice(0, index).select {|pi| $nerds[pi] < point["qi"]}
+  pi = $sorted_nerds.delete_at(index)
+  $nerds.delete(pi)
 end
 
 def join(point)
@@ -37,12 +40,45 @@ def join(point)
   remove(point)
   add(point)
   
-  $nerds.length
+  $sum += $nerds.length
 end
 
-$nerds = {}
-$sorted_nerds = []
-join({ "pi" => 72, "qi" => 50})
-join({ "pi" => 57, "qi" => 67})
-join({ "pi" => 74, "qi" => 55})
-join({ "pi" => 64, "qi" => 60})
+def run()
+  gets.to_i.times { |i|
+    $nerds = {}
+    $sorted_nerds = []
+    $sum = 0
+    
+    gets.to_i.times { |j|
+      point = gets.split.map(&:to_i)
+      join({ "pi" => point[0], "qi" => point[1]})
+    }
+
+    puts $sum
+  }
+end
+
+run()
+
+
+=begin
+$nerds = {}                      # => {}
+$sorted_nerds = []               # => []
+$sum = 0                         # => 0
+join({ "pi" => 72, "qi" => 50})  # => 1
+join({ "pi" => 57, "qi" => 67})  # => 3
+join({ "pi" => 74, "qi" => 55})  # => 5
+join({ "pi" => 64, "qi" => 60})  # => 8
+
+$nerds = {}                    # => {}
+$sorted_nerds = []             # => []
+$sum = 0                       # => 0
+join({ "pi" => 1, "qi" => 5})  # => 1
+join({ "pi" => 2, "qi" => 4})  # => 3
+join({ "pi" => 3, "qi" => 3})  # => 6
+join({ "pi" => 4, "qi" => 2})  # => 10
+join({ "pi" => 5, "qi" => 1})  # => 15
+=end
+
+
+
