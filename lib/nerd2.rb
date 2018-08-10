c7047c00248e7ae0742e01ef0da66f42fbf675d0
yongseok kang <yongseok.kang@navercorp.com>
2018-08-10 11:01:42 +0900

    Add nerd2

--- lib/nerd2.rb
+++ lib/nerd2.rb
@@ -1,22 +1,18 @@
 
 def contain?(point)
-  index = $sorted_nerds.bsearch_index {|pi| point["pi"] < pi }
-  if $sorted_nerds.length == 0 || index == nil
-    return false
-  end
-
-  $sorted_nerds[index..-1].select {|pi| point["qi"] < $nerds[pi]}.length > 0
+  $sorted_nerds.bsearch_index {|pi| point["pi"] < pi && point["qi"] < $nerds[pi]} != nil
 end
 
 def add(point)
   $nerds[point["pi"]] = point["qi"]
 
-  index = 0
-  if $sorted_nerds.length > 0
-    index = $sorted_nerds.index {|pi| pi > point["pi"] }
-    index = index == nil ? $sorted_nerds.length : index
+  index = $sorted_nerds.bsearch_index {|pi| pi > point["pi"]}
+  if index != nil
+    $sorted_nerds.insert(index, point["pi"])
+  else
+    $sorted_nerds.push(point["pi"])
   end
-  $sorted_nerds.insert(index, point["pi"])
+  
 end
 
 def remove(point) 
@@ -34,24 +30,24 @@ end
 
 def join(point)
   if contain?(point)
-    return $nerds.length()
+    return $sorted_nerds.length
   end
 
   remove(point)
   add(point)
   
-  $sum += $nerds.length
+  $sum += $sorted_nerds.length
 end
 
 def run()
-  gets.to_i.times { |i|
+  gets.to_i.times {
     $nerds = {}
     $sorted_nerds = []
     $sum = 0
     
-    gets.to_i.times { |j|
+    gets.to_i.times {
       point = gets.split.map(&:to_i)
-      join({ "pi" => point[0], "qi" => point[1]})
+      join({"pi" => point[0], "qi" => point[1]})
     }
 
     puts $sum
@@ -61,24 +57,23 @@ end
 run()
 
 
-=begin
-$nerds = {}                      # => {}
-$sorted_nerds = []               # => []
-$sum = 0                         # => 0
-join({ "pi" => 72, "qi" => 50})  # => 1
-join({ "pi" => 57, "qi" => 67})  # => 3
-join({ "pi" => 74, "qi" => 55})  # => 5
-join({ "pi" => 64, "qi" => 60})  # => 8
 
-$nerds = {}                    # => {}
-$sorted_nerds = []             # => []
-$sum = 0                       # => 0
-join({ "pi" => 1, "qi" => 5})  # => 1
-join({ "pi" => 2, "qi" => 4})  # => 3
-join({ "pi" => 3, "qi" => 3})  # => 6
-join({ "pi" => 4, "qi" => 2})  # => 10
-join({ "pi" => 5, "qi" => 1})  # => 15
-=end
+$nerds = {}
+$sorted_nerds = []
+$sum = 0
+join({ "pi" => 72, "qi" => 50})
+join({ "pi" => 57, "qi" => 67})
+join({ "pi" => 74, "qi" => 55})
+join({ "pi" => 64, "qi" => 60})
 
+
+$nerds = {}
+$sorted_nerds = []
+$sum = 0
+join({ "pi" => 1, "qi" => 5})
+join({ "pi" => 2, "qi" => 4})
+join({ "pi" => 3, "qi" => 3})
+join({ "pi" => 4, "qi" => 2})
+join({ "pi" => 5, "qi" => 1})
 
 
