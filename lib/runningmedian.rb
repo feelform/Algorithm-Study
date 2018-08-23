6843de37b19dae940013e8970f5e6fcc705a6dff
Yongseok Kang <feelform@gmail.com>
2018-08-23 11:12:48 +0900

    Change Main algorithm

--- lib/runningmedian.rb
+++ lib/runningmedian.rb
@@ -1,38 +1,52 @@
-require 'set'  # => true
+require 'set'
 
 def running_median(n, a, b)
-  input = 1983                  # => 1983
-  sum = input                   # => 1983
-  seq = SortedSet.new([input])  # => #<SortedSet: {1983}>
+  input = 1983
+  sum = input
+  seq = SortedSet.new([input])
+  left_seq = []
+  right_seq = []
+  median = input
   
-  (n-1).times {                         # => 9
-    input = (input * a + b) % 20090711  # => 1983, 1983, 1983, 1983, 1983, 1983, 1983, 1983, 1983
+  (n-1).times {
+    input = (input * a + b) % 20090711
 
-    seq.add(input)        # => #<SortedSet: {1983}>, #<SortedSet: {1983}>, #<SortedSet: {1983}>, #<SortedSet: {1983}>, #<SortedSet: {1983}>, #<SortedSet: {1983}>, #<SortedSet: {1983}>, #<SortedSet: {1983}>, #<SortedSet: {1983}>
-    seq_array = seq.to_a  # => [1983],               [1983],               [1983],               [1983],               [1983],               [1983],               [1983],               [1983],               [1983]
+    if input > median
+      right_seq.push input
+    else
+      left_seq.push input
+    end
 
-    mid_offset = seq_array.size % 2 == 0 ? -1 : 0       # => 0,    0,    0,    0,    0,    0,    0,    0,    0
-    mid_index = (seq_array.size / 2).to_i + mid_offset  # => 0,    0,    0,    0,    0,    0,    0,    0,    0
-    mid = seq_array[mid_index]                          # => 1983, 1983, 1983, 1983, 1983, 1983, 1983, 1983, 1983
+    diff = left_seq.size - right_seq.size
 
-    sum = (sum + mid) % 20090711  # => 3966, 5949, 7932, 9915, 11898, 13881, 15864, 17847, 19830
-  }                               # => 9
+    if diff > 0
+      right_seq.push median
+      median = left_seq.pop
+    elsif diff < 0
+      left_seq.push median
+      median = right_seq.pop
+    end
 
-  sum  # => 19830
-end    # => :running_median
+    median
+
+    sum = (sum + median) % 20090711
+  }
+
+  sum
+end
 
 def run()
-  gets.to_i.times {                                 # => 0
+  gets.to_i.times {
     args = gets.split.map(&:to_i)
     puts running_median(args[0], args[1], args[2])
-  }                                                 # => 0
-end                                                 # => :run
+  }
+end
 
-run()  # => 0
+run()
 
 
-running_median(10, 1, 0)  # => 19830
-#running_median(10, 1, 1)
+running_median(10, 1, 0)
+running_median(10, 1, 1)
 #running_median(10000, 1273, 4936)
 
 
